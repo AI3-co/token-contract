fc4c45a49d14427b3d3e334f09d462a08b658422
apinanyogaratnam <apinanapinan@icloud.com>
2023-02-26 17:13:35 -0500

    fix: contract

--- contracts/AI3C.sol
+++ contracts/AI3C.sol
@@ -1,20 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract AI3C is ERC20, ERC20Permit, AccessControl, ERC20Burnable {
+contract AI3C is ERC20, ERC20Burnable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+	address owner;
 
-    constructor() ERC20("Ai3.co", "AI3C") ERC20Permit("Ai3.co") {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _mint(_msgSender(), 10_000_000 * 1e18);
+    constructor() ERC20("Ai3.co", "AI3C") {
+        _mint(msg.sender, 10_000_000 * 1e18);
+		owner = msg.sender;
     }
 
-    function mint(address _to, uint256 _amount) external onlyRole(MINTER_ROLE) {
+    function mint(address _to, uint256 _amount) external {
+		require(owner == msg.sender);
         _mint(_to, _amount);
     }
+
+	function setOwner(address newOwner) external {
+		require(owner == msg.sender);
+		owner = newOwner;
+	}
 }

a32a38b1e135db4e19e5677029e176d1dc602590
apinanyogaratnam <apinanapinan@icloud.com>
2023-02-26 12:36:40 -0500

    refactor: renamed contract

--- contracts/AI3C.sol
+++ contracts/AI3C.sol
@@ -6,10 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract CODE is ERC20, ERC20Permit, AccessControl, ERC20Burnable {
+contract AI3C is ERC20, ERC20Permit, AccessControl, ERC20Burnable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Ai3.co", "AI3CO") ERC20Permit("Ai3.co") {
+    constructor() ERC20("Ai3.co", "AI3C") ERC20Permit("Ai3.co") {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _mint(_msgSender(), 10_000_000 * 1e18);
     }
